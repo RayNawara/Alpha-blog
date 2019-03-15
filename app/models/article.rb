f8f9047f561eb9b05806d45b66dae95b03fc2474
Ray Nawara <rjnawara@gmail.com>
2019-03-15 14:33:10 -0400

    catgory error

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many article_categories
+  has_many :article_categories
   has_many :catgories, through: :article_categories
   validates :title, presence: true, length: {minimum:3, maximum:50}
   validates :description, presence: true, length: {minimum:10, maximum:300}
